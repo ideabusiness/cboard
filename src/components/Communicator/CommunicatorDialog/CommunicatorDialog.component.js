94d72c2538bf0e25281b53f2304f9c184ccbfdba
Martin Di Palma <tincho.dipalma@gmail.com>
2018-10-17 10:52:55 -0300

    deployment test

--- src/components/Communicator/CommunicatorDialog/CommunicatorDialog.component.js
+++ src/components/Communicator/CommunicatorDialog/CommunicatorDialog.component.js
@@ -86,7 +86,7 @@ const CommunicatorDialog = ({
           {!loading && (
             <React.Fragment>
               {selectedTab === TAB_INDEXES.COMMUNICATOR_BOARDS && (
-                <div className="CommunicatorDialog__communicatorData CommunicatorDialog__communicatorData__container">
+                <div className="CommunicatorDialog__communicatorData">
                   <React.Fragment>
                     <div className="CommunicatorDialog__communicatorData__title">
                       {communicator.name}
